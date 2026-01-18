15e707162c328e507ad2466121282f4ff46a3ce5
kristinecosteros-kristinecosteros@gmail.com <kristinecosteros-kristinecosteros@gmail.com>
2026-01-18 03:40:44 +0000

    Add ConsoleApp1 console app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿class task1
+{
+     static void Main(string[] args)
+    {
+        // This line prints "Kamusta Mundo"
+        Console.WriteLine("Kamusta Mundo");
+        
+        
+        
+    }
+}
